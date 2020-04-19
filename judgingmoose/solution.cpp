249f75ddea87b29c1f931eefe6d8df2d96d205d8
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-04-19 17:13:46 +0200

    16 c++

---
+++ judgingmoose/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int x,y;
+  cin >> x >> y;
+  if(x == y and x+y != 0) cout << "Even " << x * 2 << endl;
+  else if(x != y and x+y != 0) cout << "Odd " << max(x,y) * 2 << endl;
+  else cout << "Not a moose" << endl;
+}
