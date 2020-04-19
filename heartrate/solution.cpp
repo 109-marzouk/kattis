a5c3eb6825df045c84a944fd51aa0306144230d4
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-04-19 18:24:33 +0200

    22 c++

--- heartrate/solution.cpp
+++ heartrate/solution.cpp
@@ -1,15 +1,14 @@
 // https://open.kattis.com/problems/heartrate
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
   int r; cin >> r;
-  string result[r];
+  string result;
   float b,p;
   for(int i = 0; i < r; i++){
     cin >> b >> p;
-    result[i] = to_string(((b-1)*60)/p) + " " +  to_string((b*60)/p) + " " +  to_string(((b+1)*60)/p);
+    result += to_string(((b-1)*60)/p) + " " +  to_string((b*60)/p) + " " +  to_string(((b+1)*60)/p) + "\n";
   }
-  for(auto z : result) cout<< z << endl;
+  cout<< result << endl;
 }
