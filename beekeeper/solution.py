e7400954e3e31bbdb4d266941d1c7c9f6a6dd047
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-04-24 11:37:01 +0200

    43 python 2

---
+++ beekeeper/solution.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/beekeeper
+while True:
+  n = input()
+  if n == 0: break
+  d = {}
+  for x in xrange(n):
+    s = raw_input()
+    c_i = 0
+    r = 0
+    for e in "aeiouy": r += s.count(e*2)
+    d[r] = s
+  print d[max(d.keys())]
